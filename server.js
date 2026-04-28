e5d2514f2f34742bb6598d9bc366857e724cb91b
jackosnwildy <jackosnwildy@gmail.com>
2026-04-27 20:47:03 -0500

    Fix broken console.log lines

--- server.js
+++ server.js
@@ -775,8 +775,8 @@ async function startServer() {
   initWebSocketServer(server);
   
   server.listen(PORT, () => {
-    console.log(`Server running on http:
-    console.log(`WebSocket server running on ws:
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`WebSocket server running on ws://localhost:${PORT}/ws`);
     console.log(`Admin IDs: ${ADMIN_IDS.join(', ')}`);
     console.log('Make sure to set DISCORD_CLIENT_ID and DISCORD_CLIENT_SECRET environment variables');
   });
